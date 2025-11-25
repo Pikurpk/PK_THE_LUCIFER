082441af409d3a2c7696d60dc75dc081a83d809d
Pikurpk <mdfoysalislam785@gmail.com>
2025-11-26 00:58:24 +0600

    Add files via upload

--- login.sh
+++ login.sh
@@ -1,3 +1,37 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "Login system placeholder"
+# USERNAME & PASSWORD
+username="pkthelucifer"
+password="Lucifer@143"
+
+# Clear Screen
+clear
+
+# Banner
+echo -e "\e[1;31m
+██╗     ██╗   ██╗ ██████╗██╗███████╗███████╗██████╗ 
+██║     ██║   ██║██╔════╝██║██╔════╝██╔════╝██╔══██╗
+██║     ██║   ██║██║     ██║█████╗  █████╗  ██████╔╝
+██║     ██║   ██║██║     ██║██╔══╝  ██╔══╝  ██╔══██╗
+███████╗╚██████╔╝╚██████╗██║██║     ███████╗██║  ██║
+╚══════╝ ╚═════╝  ╚═════╝╚═╝╚═╝     ╚══════╝╚═╝  ╚═╝
+\e[0m"
+
+echo -e "\e[1;32m          Welcome Lucifer\e[0m"
+echo -e "\e[1;34m            Powered by Foysal\e[0m"
+echo ""
+echo "-------------------------------------------"
+
+# Login Prompt
+read -p "Username: " input_user
+read -s -p "Password: " input_pass
+echo
+
+if [[ "$input_user" == "$username" && "$input_pass" == "$password" ]]; then
+    echo -e "\e[1;32mLogin Successful! Welcome Lucifer.\e[0m"
+    sleep 1
+    clear
+else
+    echo -e "\e[1;31mWrong Username or Password!\e[0m"
+    exit
+fi
